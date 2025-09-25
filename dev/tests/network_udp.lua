11d9f9562e73b56b06abb81891046de4237de0e5
MihailRis <mexaruc@gmail.com>
2025-09-25 12:20:41 +0300

    fix test

--- dev/tests/network_udp.lua
+++ dev/tests/network_udp.lua
@@ -1,28 +1,28 @@
 math.randomseed(43172)
 for i = 1, 15 do
-    debug.print(string.format("iteration %s", i))
+    debug.log(string.format("iteration %s", i))
     local complete = false
 
     local server = network.udp_open(8645 + i, function (address, port, data, srv)
-        debug.print(string.format("server received %s byte(s) from %s:%s", #data, address, port))
+        debug.log(string.format("server received %s byte(s) from %s:%s", #data, address, port))
         srv:send(address, port, "pong")
     end)
 
     app.tick()
     network.udp_connect("localhost", 8645 + i, function (data)
-        debug.print(string.format("client received %s byte(s) from server", #data))
+        debug.log(string.format("client received %s byte(s) from server", #data))
         complete = true
     end, function (socket)
-        debug.print("udp socket opened")
+        debug.log("udp socket opened")
         start_coroutine(function()
-            debug.print("udp data-sender started")
+            debug.log("udp data-sender started")
             for k = 1, 15 do
                 local payload = ""
                 for j = 1, 16 do
                     payload = payload .. math.random(0, 9)
                 end
                 socket:send(payload)
-                debug.print(string.format("sent packet %s (%s bytes)", k, #payload))
+                debug.log(string.format("sent packet %s (%s bytes)", k, #payload))
                 coroutine.yield()
             end
             socket:close()
